dd3ed80f8c6717f2179440c3e700324da0d1f53d
alexinea <forerunner@outlook.com>
2020-06-05 14:18:07 +0800

    Add non-generic object pool entry.

---
+++ samples/Samples.BasicSample.NonGeneric/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using Cosmos.Disposables.ObjectPools;
+using Cosmos.Disposables.ObjectPools.Managed;
+
+namespace Samples.BasicSample.NonGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var managedModelType = typeof(CustomModel);
+            var type = typeof(SampleModel);
+
+            //var pool = new ObjectPool(type, 10, CreateObject, OnGetObject);
+
+            //ObjectPoolManager.Create("OK", 10, CreateObject, OnGetObject);
+
+            // ObjectPoolManager.ManagedModels.Register(managedModelType);
+            // ObjectPoolManager.ManagedModels.Create(managedModelType, type, "OK", 10, CreateObject, OnGetObject);
+
+            ObjectPoolManager.NonGeneric.Managed(managedModelType).Register();
+            ObjectPoolManager.NonGeneric.Managed(managedModelType).Create(type, "OK", 10, CreateObject, OnGetObject);
+
+            //var pool = ObjectPoolManager.Get<SampleModel>();
+
+            var pool = ObjectPoolManager.ManagedModels.Get(managedModelType, type, "OK");
+            //var pool = ObjectPoolManager.ManagedModels.Get<SampleModel, CustomModel>("OK");
+
+            //var pool = ObjectPoolManager.Managed<CustomModel>.Get<SampleModel>("OK");
+
+            for (var i = 0; i < 100; i++)
+            {
+                new Thread(() =>
+                {
+                    for (var j = 0; j < 1_000; j++)
+                    {
+                        var item = pool.Get();
+                        //Console.WriteLine($"ThreadId={Thread.CurrentThread.ManagedThreadId}, Value={item.Value.Value}");
+                        pool.Return(item);
+                    }
+
+                    Console.WriteLine(pool.StatisticsFully);
+                }).Start();
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static SampleModel CreateObject() => new SampleModel();
+
+        private static void OnGetObject(ObjectOut model)
+        {
+            if (DateTime.Now.Subtract(model.LastGetTime).TotalSeconds > 3)
+            {
+                ((SampleModel) model.Value).Value += " +3sec";
+            }
+        }
+    }
+
+    public class SampleModel
+    {
+        public string Value { get; set; } = DateTime.Now.ToString("yyyy MM dd HH mm ss fff");
+    }
+
+    public class CustomModel : ObjectPoolManagedModel { }
+}
---
+++ src/Cosmos.ObjectPools/Cosmos/Disposables/ObjectPools/Extensions.Policy.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace Cosmos.Disposables.ObjectPools
+{
+    /// <summary>
+    /// Extensions for policy
+    /// </summary>
+    public static class PolicyExtensions
+    {
+        /// <summary>
+        /// Register this policy for object pool
+        /// </summary>
+        /// <param name="policy"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void Register<T>(this IPolicy<T> policy)
+        {
+            if (policy is null)
+                throw new ArgumentNullException(nameof(policy));
+            ObjectPoolManager.Create(policy);
+        }
+
+        /// <summary>
+        /// Register this policy for object pool safety
+        /// </summary>
+        /// <param name="policy"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void SafeRegister<T>(this IPolicy<T> policy)
+        {
+            if (policy is null)
+                return;
+            if (ObjectPoolManager.Contains<T>())
+                return;
+            ObjectPoolManager.Create(policy);
+        }
+
+        /// <summary>
+        /// Try register this policy for object pool.
+        /// </summary>
+        /// <param name="policy"></param>
+        /// <param name="pool"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool TryRegister<T>(this IPolicy<T> policy, out IObjectPool<T> pool)
+        {
+            try
+            {
+                if (policy is null)
+                    throw new ArgumentNullException(nameof(policy));
+                pool = ObjectPoolManager.Create(policy);
+                return true;
+            }
+            catch
+            {
+                pool = default;
+                return false;
+            }
+        }
+    }
+}
